675ee498764e606e6d85d027fc61e615f82dbf70
hri-gh <hrithikgh.edu@gmail.com>
2024-04-25 10:09:08 +0530

    "Use useSkills hook to fetch skills instead of prismadb in SkillsPage component"
    This commit updates the SkillsPage component to use the `useSkills` hook to fetch skills instead of using `prismad

--- app/admin/(routes)/skills/page.tsx
+++ app/admin/(routes)/skills/page.tsx
@@ -1,22 +1,29 @@
+"use client"
+
 import { format } from "date-fns";
 import React from 'react'
 
 import prismadb from '@/lib/prismadb'
 
 import { SkillClient } from "./components/client";
+import { useSkills } from "@/hooks/get-skills";
 
 export type SkillCardData = {
   id: string;
   name: string;
   image: string;
+  updatedAt: string;
+  createdAt: string;
 }
 
-const SkillsPage = async () => {
-  const skills = await prismadb.skill.findMany()
+const SkillsPage = () => {
+  const [data, loading, error] = useSkills()
 
-  const formattedSkills: SkillCardData[] = skills.map((item) => ({
+  // const skills = await prismadb.skill.findMany()
+
+  const formattedSkills: SkillCardData[] = data.map((item:any) => ({
     id: item.id,
-    name:item.name,
+    name: item.name,
     image: item.imageUrl,
     updatedAt: format(item.updatedAt, 'MMMM do, yyyy'),
     createdAt: format(item.createdAt, 'MMMM do, yyyy'),
@@ -26,6 +33,8 @@ const SkillsPage = async () => {
     <>
       <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
+          {loading && (<p>Loading...</p>)}
+          {error && (<p>{error}</p>)}
           <SkillClient data={formattedSkills} />
         </div>
       </div>
